c85955c74ae7fb19154b8f87f145b0b9f9ad9cd1
jskadomoto <jonesseiji9@gmail.com>
2024-08-20 23:01:11 -0300

    build: add sql server pkg

--- obj/Debug/net8.0/i-want-app-dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/i-want-app-dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("i-want-app-dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a56f5303ddce8b9e21ac34fae42816180e94b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae05c11f351681e1cc56bede4bd303ff5d3449a")]
 [assembly: System.Reflection.AssemblyProductAttribute("i-want-app-dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("i-want-app-dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
