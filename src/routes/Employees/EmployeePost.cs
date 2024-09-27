6b35c0b3b280edd954c39abd88cf838b2de0ca04
jskadomoto <jonesseiji9@gmail.com>
2024-09-27 13:03:29 -0300

    refactor: list all claims

--- src/routes/Employees/EmployeePost.cs
+++ src/routes/Employees/EmployeePost.cs
@@ -12,23 +12,22 @@ public class EmployeePost
     var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
 
     if (!result.Succeeded)
-    {
       return Results.BadRequest(result.Errors.First());
+
+    var userClaims = new List<Claim>
+    {
+      new Claim("EmployeeCode", employeeRequest.EmployeeCode),
+      new Claim("Name", employeeRequest.Name),
+    };
+
+    foreach (var claim in userClaims)
+    {
+      var claimResult = userManager.AddClaimAsync(user, claim).Result;
+
+      if (!claimResult.Succeeded)
+        return Results.BadRequest(claimResult.Errors.First());
     }
 
-    var claimResult = userManager.AddClaimAsync(user, new Claim("EmployeeCode", employeeRequest.EmployeeCode)).Result;
-
-    if (!claimResult.Succeeded)
-    {
-      return Results.BadRequest(result.Errors.First());
-    }
-
-    claimResult = userManager.AddClaimAsync(user, new Claim("EmployeeName", employeeRequest.Name)).Result;
-
-    if (!claimResult.Succeeded)
-    {
-      return Results.BadRequest(result.Errors.First());
-    }
 
     return Results.Created($"/employees/{user.Id}", user.Id);
 
