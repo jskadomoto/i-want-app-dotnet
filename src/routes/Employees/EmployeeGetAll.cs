faa825de5918cbd29df554d78f5fe02c62546f5d
jskadomoto <jonesseiji9@gmail.com>
2024-09-27 15:39:13 -0300

    feat: add dapper

--- src/routes/Employees/EmployeeGetAll.cs
+++ src/routes/Employees/EmployeeGetAll.cs
@@ -1,27 +1,40 @@
-using System.Security.Claims;
-using IWantApp.Domain;
-using Microsoft.AspNetCore.Identity;
+using Dapper;
+using Microsoft.Data.SqlClient;
 
 public class EmployeeGetAll
 {
   public static string Template => "/employees";
   public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
   public static Delegate Handler => Action;
-  public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+  public static IResult Action(int? page, int? rows, IConfiguration configuration)
   {
-    var users = userManager.Users.Skip(Pagination.SkipPage(page, rows)).Take(rows).ToList();
-    var employees = new List<EmployeeResponse>();
+    page ??= 1;
 
-    foreach (var item in users)
-    {
-      var claims = userManager.GetClaimsAsync(item).Result;
-      var claimName = claims.FirstOrDefault(c => c.Type == "Name");
-      var userName = claimName != null ? claimName.Value : string.Empty;
+    if (rows == null || rows > 10) {
+      rows = 10;
+      var warning = "O número máximo de registros por página é 10. A busca foi limitada.";
 
-      employees.Add(new EmployeeResponse(item.Email, userName));
+      return Results.Ok(new { Warning = warning, Employees = GetEmployees(page.Value, rows.Value, configuration) });
     }
+    var employees = GetEmployees(page.Value, rows.Value, configuration);
 
     return Results.Ok(employees);
+  }
+  private static IEnumerable<EmployeeResponse> GetEmployees(int page, int rows, IConfiguration configuration)
+  {
 
+    var db = new SqlConnection(configuration["ConnectionString:IWantDb"]);
+
+    var query = @"select Email, ClaimValue as Name
+        from AspNetUsers u
+        inner join AspNetUserClaims c
+        on u.Id = c.UserId
+        and ClaimType = 'Name'
+        order by Name
+        OFFSET (@page - 1) * @rows ROWS
+        FETCH NEXT @rows ROWS ONLY
+      ";
+
+    return db.Query<EmployeeResponse>(query, new { page, rows });
   }
 }
