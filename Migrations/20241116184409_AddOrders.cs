b774889fa68e61e6bf66dfd9797e7938c07d34af
jskadomoto <jonesseiji9@gmail.com>
2024-11-16 15:46:57 -0300

    feat: add order in db context

---
+++ Migrations/20241116184409_AddOrders.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace i_want_app_dotnet.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Order_OrdersId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Orders_OrdersId",
+                table: "OrderProducts",
+                column: "OrdersId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Orders_OrdersId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Order_OrdersId",
+                table: "OrderProducts",
+                column: "OrdersId",
+                principalTable: "Order",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
