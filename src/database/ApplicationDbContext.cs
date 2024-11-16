cba694e8da7dc158f90fc826e8a7d6bcb11bc431
jskadomoto <jonesseiji9@gmail.com>
2024-11-16 15:43:53 -0300

    feat: add orders

--- src/database/ApplicationDbContext.cs
+++ src/database/ApplicationDbContext.cs
@@ -4,7 +4,9 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
 {
   public DbSet<Product> Products { get; set; }
   public DbSet<Category> Categories { get; set; }
-  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+  public DbSet<Order> Orders { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
   protected override void OnModelCreating(ModelBuilder builder)
   {
     base.OnModelCreating(builder);
